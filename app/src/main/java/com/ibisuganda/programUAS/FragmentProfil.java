03aeaec570b2bf2ae79a5bb590d38202297e20c0
ibisuganda <felangm48@gmail.com>
2019-08-17 14:53:21 +0700

    uas

---
+++ app/src/main/java/com/ibisuganda/programUAS/FragmentProfil.java
@@ -0,0 +1,29 @@
+package com.ibisuganda.programUAS;
+//16 agustus 2019, 10116151, Ibi Patria Suganda, IF-4
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentProfil extends Fragment {
+
+
+    public FragmentProfil() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profil, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/ibisuganda/programUAS/Fragment_account.java
@@ -0,0 +1,24 @@
+package com.ibisuganda.programUAS;
+//16 agustus 2019, 10116151, Ibi Patria Suganda, IF-4
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_account extends Fragment {
+
+
+    public Fragment_account() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+
+}
